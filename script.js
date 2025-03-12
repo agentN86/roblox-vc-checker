7a032a513315d8651e00851af7ffb7a39a0ff10c
agentN86 <agentn86@duck.com>
2025-03-11 19:35:50 -0500

    fixed it, yadaydayda

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-fetch("https://voice.roblox.com/v1/settings")
-    .then((res) => res.json())
-    .then((data) => {
+function dostuff() {
+    // basically grab the json file from the input
+    // do stuff
+    // and then return it back to the user
+    var fileList = document.getElementById('jsonFile');
+    var data = fileList.files[0];
+    var reader = new FileReader();
 
-        switch (data.isBanned) {
-            case true:
+    reader.onload = function (event) {
+
+        try {
+            var data = JSON.parse(event.target.result);
+            if (data.isBanned) {
                 console.log("banned");
                 var reasons = [
                     "NOTBANNED", // 0 (not banned)
@@ -11,21 +18,34 @@ fetch("https://voice.roblox.com/v1/settings")
                     "unknown reasons", // 2
                     "unknown reasons", // 3
                     "unknown reasons", // 4
-                    "harassing/bullying others", //5
-                    "self-harm/threating others" //6
-                ]
+                    "harassing/bullying others", // 5
+                    "self-harm/threating others" // 6
+                ];
+
                 document.getElementById('reason').innerText = " for " + reasons[data.banReason];
                 document.getElementById('until').style.display = "block";
                 const stamp = new Date(data.bannedUntil.Seconds * 1000);
-                document.getElementById('timestamp').innerText = `${stamp.toDateString()}, ${stamp.toTimeString()}`
-                break;
-            case false:
+                document.getElementById('timestamp').innerText = `${stamp.toDateString()}, ${stamp.toTimeString()}`;
+
+            } else {
+
                 console.log("not banned");
                 document.getElementById('result').innerText = "not";
                 var stuff = ["go enjoy a nice dinner, or whatever you can do", "keep it moving"];
                 document.getElementById('oneliner').innerText = stuff[Math.floor(Math.random() * stuff.length)];
-                break;
 
+            }
+
+        } catch (error) {
+            console.error("Error parsing JSON:", error);
         }
 
-    });
+    };
+
+
+    reader.onerror = function (event) {
+        console.error("Error reading file:", event.target.error);
+    };
+
+    reader.readAsText(data);
+}
